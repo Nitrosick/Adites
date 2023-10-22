f98ba858430fdc71144e874afc56489e62a47b38
Nikita Buzhinskiy <n.buzhinskiy@ruvents.com>
2023-10-22 15:15:53 +0400

    Replaced pictures and persons

--- const/persons.js
+++ const/persons.js
@@ -1,11 +1,11 @@
 export const persons = [
   {
-    id: 'ilya-vilvovskiy',
-    name: 'Ilya Vilvovskiy',
+    id: 'berator-1',
+    name: 'Berator 1',
     city: { en: 'Amberg', de: 'Amberg', hr: 'Amberg' },
-    email: 'ilya.vilvovskiy@adites.de',
-    phone: '+49 (0) 151 68565004',
-    photo: 'vilvovskiy.jpg',
+    email: 'berator1@adites.de',
+    phone: '+49 (0) 000 00000000',
+    photo: 'berator.jpg',
     berator: { en: 'From Amberg Berator', de: 'Aus Amberg Berator', hr: 'Iz Amberga Beratora' },
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24678.82072785863!2d11.838741381739188!3d49.44593291896166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a02bb2583a38d1%3A0x1c1eda35f4c536d0!2zOTIyMjQg0JDQvNCx0LXRgNCzLCDQk9C10YDQvNCw0L3QuNGP!5e0!3m2!1sru!2sge!4v1697185292466!5m2!1sru!2sge',
     description: {
@@ -15,12 +15,12 @@ export const persons = [
     }
   },
   {
-    id: 'kevin-dambrowski',
-    name: 'Kevin Dambrowski',
+    id: 'berator-2',
+    name: 'Berator 2',
     city: { en: 'Nuremberg', de: 'Nürnberg', hr: 'Nirnberg' },
-    email: 'KevinDambrowski@gmail.com',
-    phone: '+49 (0) 174 9544379',
-    photo: 'dambrowski.jpg',
+    email: 'berator2@adites.de',
+    phone: '+49 (0) 000 00000000',
+    photo: 'berator.jpg',
     berator: { en: 'From Nuremberg Berator', de: 'Aus Nürnberg Berator', hr: 'Iz Nirnberškog Beratora' },
     map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58691.48299461013!2d11.051818967201271!3d49.4500887154333!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479f57aeb5b61cd3%3A0xdd5daf85a98c21b7!2z0J3RjtGA0L3QsdC10YDQsywg0JPQtdGA0LzQsNC90LjRjw!5e0!3m2!1sru!2sge!4v1697185691022!5m2!1sru!2sge',
     description: {
