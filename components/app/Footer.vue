1561fff88c91b1aa4a14624b3a73d75948a25add
Nikita Buzhinskiy <n.buzhinskiy@ruvents.com>
2023-12-21 10:33:08 +0400

    Footer changed

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -1,20 +1,41 @@
 <template>
   <div class="footer">
     <div class="footer-content wrapper">
-      <img
-        src="@/assets/images/sun_panel.png"
-        alt="sun panel"
-        class="footer-icon"
+      <h2
+        class="footer-title"
+        v-html="translates[ln].title"
+      />
+      <span class="footer-gmbh">
+        ADITES GmbH
+      </span>
+      <i class="footer-address">
+        Vinodolska ul. 27, 51260<br>Crikvenica, Hrvatska
+      </i>
+      <a
+        href="tel:+38551452461"
+        class="footer-link"
       >
-      <div class="footer-text">
-        <h2>ADITES GmbH</h2>
-        <span>Vinodolska ul. 27, 51260, Crikvenica, Hrvatska</span>
-      </div>
+        TEL +385 514 524 61
+      </a>
+      <a
+        href="mailto:info@adites.hr"
+        class="footer-link"
+      >
+        info@adites.hr
+      </a>
     </div>
   </div>
 </template>
 
 <script setup>
+const store = useMainStore()
+const ln = computed(() => store.language)
+
+const translates = {
+  en: { title: 'Contacts' },
+  de: { title: 'Kontakt' },
+  hr: { title: 'Kontakti' }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -23,16 +44,25 @@
 
   &-content {
     display: flex;
-    align-items: center;
-    gap: 1rem;
+    flex-direction: column;
     padding: 3rem 1rem;
+    color: $color-white;
   }
 
-  &-text {
-    h2 {
-      text-align: left;
-    }
+  &-title {
+    margin-bottom: 1.5rem;
+    text-align: left;
+  }
 
+  &-gmbh {
+    font-weight: 600;
+  }
+
+  &-address {
+    margin-bottom: 1.5rem;
+  }
+
+  &-link {
     color: $color-white;
   }
 }
